f7dbb581b9e2a4dd0910e85d8f8e775a27e83734
luisdasilvahenrique <luisenriklh1177027@gmail.com>
2022-03-06 20:34:00 -0300

    componentização da aplicação

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,18 @@
 export function SideBar() {
-  // Complete aqui
+  <nav className="sidebar">
+        <span>Watch<p>Me</p></span>
+
+        <div className="buttons-container">
+          {genres.map(genre => (
+            <Button
+              key={String(genre.id)}
+              title={genre.title}
+              iconName={genre.name}
+              onClick={() => handleClickButton(genre.id)}
+              selected={selectedGenreId === genre.id}
+            />
+          ))}
+        </div>
+
+      </nav>
 }
